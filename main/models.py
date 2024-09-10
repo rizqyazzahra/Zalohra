ae108012e04b5c9fab5214caf36b7e62e1618e13
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-09-11 01:36:50 +0700

    Update models, views, main.html

--- main/models.py
+++ main/models.py
@@ -4,3 +4,4 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- main/views.py
+++ main/views.py
@@ -3,7 +3,32 @@ from .models import Product
 
 # Create your views here.
 def show_main(request):
-    products = Product.objects.all()
+    # product1 = Product.object # create object prod
+    # product = Product.objects.all()
+
+    product1 = {
+        'name': 'Naiki Air Shoes',
+        'description': 'Deskripsi Produk A',
+        'price': 50000,
+        'image_url': 'https://github.com/rizqyazzahra/Zalohra/blob/main/main/templates/images/naiki.jpeg'
+    }
+
+    product2 = {
+        'name': 'Convoso 50sShoes',
+        'description': 'Deskripsi Produk B',
+        'price': 40000,
+        'image_url': 'https://github.com/rizqyazzahra/Zalohra/blob/main/main/templates/images/convose.jpg'
+    }
+
+    product3 = {
+        'name': 'Adudu Track Jacket',
+        'description': 'Deskripsi Produk C',
+        'price': 22000,
+        'image_url': 'https://github.com/rizqyazzahra/Zalohra/blob/main/main/templates/images/adudu.jpg'
+    }
+
+    products = [product1, product2, product3]
+    
     context = {
         'apps_name' : 'Zalohra',
         'name': 'Rizqya Az Zahra Putri',
