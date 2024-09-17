58f94066ce771202feacbfed0cf96dfa3cb9873e
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-09-17 15:05:28 +0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect
 from main.forms import ProductForm
 from main.models import Product
@@ -24,3 +26,31 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
